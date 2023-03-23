ae23ba3e24edbfd00b00522f7d95f10e3bb4f28b
Deniz Kavi <dkavi@sh02-01n58.int>
2023-03-22 17:16:43 -0700

    bug fixes and req.txt

--- run_parafold_no_template.sh
+++ run_parafold_no_template.sh
@@ -122,8 +122,7 @@ fi
 
 # This bash script looks for the run_alphafold.py script in its current working directory, if it does not exist then exits
 #current_working_dir=$(pwd)
-alphafold_script="/home/users/dkavi/ParallelFold/run_alphafold.py"
-#alphafold_script=$PWD"/ParallelFold/run_alphafold_precomputed.py"
+alphafold_script=$PWD"/ParallelFold/run_alphafold.py"
 
 if [ ! -f "$alphafold_script" ]; then
     echo "Alphafold python script $alphafold_script does not exist."
